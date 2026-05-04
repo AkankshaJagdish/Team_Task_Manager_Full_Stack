e745f810adbdd28e09690e20e4e85ba6fc8c53b6
AkankshaJagdish <akankshaj2021@gmail.com>
2026-05-04 20:09:49 +0530

    api change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend-url/api"
+  baseURL: "https://teamtaskmanagerfullstack.railway.internal/api"
 });
 
 API.interceptors.request.use(req => {
