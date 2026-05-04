eb0e0a003ba6b89758807791682f8c77eabc8871
AkankshaJagdish <akankshaj2021@gmail.com>
2026-05-04 20:24:24 +0530

    respect gitignore

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import projectRoutes from "./routes/projects.js";
 import taskRoutes from "./routes/tasks.js";
+const PORT = process.env.PORT || 5000;
+
 
 dotenv.config();
 
@@ -20,6 +22,6 @@ app.use("/api/tasks", taskRoutes);
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("DB Connected");
-    app.listen(5000, () => console.log("Server running"));
+    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 })
 .catch(err => console.log(err));
