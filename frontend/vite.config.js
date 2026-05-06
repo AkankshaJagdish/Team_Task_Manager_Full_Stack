9bf3d0bbb5e245ef7173fdf58728be1d4903e6f9
AkankshaJagdish <akankshaj2021@gmail.com>
2026-05-06 18:35:01 +0530

    reattempt deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   preview: {
+    plugins: [react()],
     host: "0.0.0.0",
     port: 8080,
     allowedHosts: [
